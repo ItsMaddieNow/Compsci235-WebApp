22d6a1e87324d8af51f72467dc13ee7f5e0543a3
Ubaydullah <18622@students.mrgs.school.nz>
2023-08-30 00:01:23 +1200

    updated routes

--- games/gamelibrary/gamelibrary.py
+++ games/gamelibrary/gamelibrary.py
@@ -3,6 +3,7 @@ import math
 import games.gamelibrary.services as services
 import games.adapters.datareader.repository as repo
 from ..domainmodel.model import Game
+
 REPO = repo.repo_instance
 GAMES_PER_PAGE = 12  # 12 is a nice number divisible by 1, 2, 3 and 4, so it works nicely with Flexbox
 
@@ -24,4 +25,22 @@ def games():
                            current_page=page,
                            total_pages=pages,
                            sort_by=sort_by,
+                           genre_name=None
                            )
+
+
+@library_blueprint.route('/games/genres/<string:genre_name>')
+def games_by_genre(genre_name):
+    page = request.args.get('page', 1, type=int)
+    page_selection = services.get_games_by_genre(page, GAMES_PER_PAGE, genre_name, REPO)
+    pages = math.ceil(services.get_game_amount_by_genre(genre_name, REPO) / GAMES_PER_PAGE)
+    genres = services.get_genres(REPO)
+    return render_template('games.html',
+                           genresList=genres,
+                           gamesList=page_selection,
+                           current_page=page,
+                           total_pages=pages,
+                           selected_genre=genre_name,
+                           genre_name=genre_name,
+                           )
+
