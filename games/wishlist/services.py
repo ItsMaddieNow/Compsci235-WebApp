48a1af1388460ff193667f97c8e090c4b4f85e46
Ubaydullah <18622@students.mrgs.school.nz>
2023-09-15 00:43:09 +1200

    Created wishlist backend, services and html pages

---
+++ games/wishlist/services.py
@@ -0,0 +1,7 @@
+from games.adapters.datareader.repository import AbstractRepository
+
+def add_to_wishlist(user_id, game_id, repo: AbstractRepository)
+
+def remove_from_wishlist(user_id, game_id, repo: AbstractRepository)
+
+def get_user_wishlist(user_id, repo: AbstractRepository)
---
+++ games/wishlist/wishlist.py
@@ -0,0 +1,21 @@
+from flask import Blueprint, session, request, render_template, jsonify, flash, url_for, redirect
+import math
+import games.wishlist.services as services
+import games.adapters.datareader.repository as repo
+
+wishlist_blueprint = Blueprint(
+    'wishlist_bp', __name__
+)
+
+
+@wishlist_blueprint.route('/wishlist/add', methods=["POST"])
+def wishlist_add():
+
+
+@wishlist_blueprint.route('/wishlist/remove', methods=["POST"])
+def wishlist_remove():
+
+
+@wishlist_blueprint.route('/wishlist/', methods=["GET"])
+def wishlist():
+
