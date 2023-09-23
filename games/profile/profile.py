52178f312b706d7c211f3c9958d9237a278b8eba
Ubaydullah <18622@students.mrgs.school.nz>
2023-09-23 21:02:26 +1200

    UI: select newest oldest reviews of user in profile

--- games/profile/profile.py
+++ games/profile/profile.py
@@ -10,23 +10,29 @@ profile_blueprint = Blueprint(
     'profile_bp', __name__
 )
 
-
 @profile_blueprint.route("/profile", methods=["GET"])
 @login_required
 def profile():
-    username = session.get('username')  # Get the username from the session
-    reviews = services.get_reviews_by_user(username, repo.repo_instance)
+    username = session.get('username')
+
     if not username:
         flash('Please log in to view your profile.', 'warning')
         return redirect(url_for('auth_bp.login'))
-    user = services.get_username(username, repo.repo_instance)  # Use the get_user service function
 
+    user = services.get_username(username, repo.repo_instance)
     if not user:
         flash('User not found.', 'error')
         return redirect(url_for('home_bp.home'))
 
+    reviews = services.get_reviews_by_user(username, repo.repo_instance)
+    sort_option = request.args.get('sort', default='newest', type=str)
+
+    if sort_option == "newest":
+        reviews.reverse()
+
     return render_template(
         'profile.html',
         user=user,
-        reviews=reviews
+        reviews=reviews,
+        sort_option=sort_option
     )
