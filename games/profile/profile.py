ad8ee73c6d64abd90a32d5ab33d600d7b7961314
bilalsarwar <bsar286@aucklanduni.ac.nz>
2023-09-24 20:44:01 +1300

    Fixed user profile reviews

--- games/profile/profile.py
+++ games/profile/profile.py
@@ -4,29 +4,26 @@ import games.profile.services as services
 import games.adapters.datareader.repository as repo
 from ..domainmodel.model import Game, User
 from games.auth.authentication import login_required
-from games.reviews import services as review_services
 
 profile_blueprint = Blueprint(
     'profile_bp', __name__
 )
 
+
 @profile_blueprint.route("/profile", methods=["GET"])
 @login_required
 def profile():
-    username = session.get('username')
-
+    username = session.get('username')  # Get the username from the session
     if not username:
         flash('Please log in to view your profile.', 'warning')
         return redirect(url_for('auth_bp.login'))
-
-    user = services.get_username(username, repo.repo_instance)
+    user = services.get_username(username, repo.repo_instance)  # Use the get_user service function
+    reviews = services.get_reviews_by_user(username, repo.repo_instance)
     if not user:
         flash('User not found.', 'error')
         return redirect(url_for('home_bp.home'))
 
-    reviews = services.get_reviews_by_user(username, repo.repo_instance)
     sort_option = request.args.get('sort', default='newest', type=str)
-
     if sort_option == "newest":
         reviews.reverse()
 
